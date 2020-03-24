f78620f2186def30140104651949c384351adff1
DomeOfHeaven2017 <domeofheaven2017@outlook.com>
2020-03-24 21:04:56 +0800

    [LeetCode]增加程序员面试宝典面试题02.02

--- src/main/java/com/wx/java_code/CoderInterviews.java
+++ src/main/java/com/wx/java_code/CoderInterviews.java
@@ -1,5 +1,7 @@
 package com.wx.java_code;
 
+import com.wx.java_code.resource.ListNode;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,6 +33,29 @@ public class CoderInterviews {
             }
         }
         return true;
+    }
+
+    /**
+     * 面试题02.02
+     * 返回倒数第 k 个节点
+     * @param head 链表头结点
+     * @param k 倒数值
+     * @return 倒数第k个节点值
+     * 与《剑指Offer》面试题22一致
+     */
+    public static int kthToLast(ListNode head, int k) {
+        ListNode p = head,q = head;
+        int i = 0;
+        while (q != null) {
+            if (i < k) {
+                q = q.next;
+                i++;
+                continue;
+            }
+            p = p.next;
+            q = q.next;
+        }
+        return p.val;
     }
 
     public static void main(String[] args) {
