421343488788b50ea65e128dbf435e6806abbec1
DomeOfHeaven2017 <domeofheaven2017@outlook.com>
2020-06-21 21:02:20 +0800

    [LeetCode]增加二叉树遍历题目

--- src/main/java/com/wx/java_code/TreeProblemSet.java
+++ src/main/java/com/wx/java_code/TreeProblemSet.java
@@ -4,14 +4,19 @@ import com.wx.java_code.resource.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by wx on 20-6-7
  * Description: LeetCode 树相关问题集合
  * 问题列表：
  *   面试题 04.04. 检查平衡性{@link #isBalanced}
- *  94.二叉树的中序遍历{@link #inorderTraversal}
  *  面试题55 - I  二叉树的深度 {@link #maxDepth}
+ *  94.二叉树的中序遍历{@link #inorderTraversal}
+ *  102. 二叉树的层序遍历{@link #levelOrder}
+ *  144. 二叉树的前序遍历{@link #preorderTraversal}
+ *  145. 二叉树的后序遍历{@link #postorderTraversal}
+ *
  **/
 public class TreeProblemSet {
 
@@ -42,20 +47,101 @@ public class TreeProblemSet {
      * @param root 二叉树根节点
      * @return 中序遍历集合
      */
-    public static List<Integer> inorderTraversal(TreeNode root) {
+    public List<Integer> inorderTraversal(TreeNode root) {
         //递归
         List<Integer> list = new ArrayList<>();
-        midTraver(root, list);
+//        treeNodeTraversal(list, root, 1);
+//        return list;
         //迭代
+        Stack<TreeNode> stack = new Stack<>();
+        if (root == null) {
+            return list;
+        }
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
 
+        }
         return list;
     }
 
-    private static void midTraver(TreeNode root, List<Integer> list) {
-        if (root != null) {
-            list.add(root.val);
-            midTraver(root.left, list);
-            midTraver(root.right, list);
+    /**
+     * 102. 二叉树的层序遍历
+     * @param root 二叉树根节点
+     * @return 层序遍历集合
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+    }
+
+    /**
+     * 144. 二叉树的前序遍历
+     * @param root 二叉树根节点
+     * @return 前序遍历集合
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        //递归
+        List<Integer> list = new ArrayList<>();
+//        treeNodeTraversal(list, root, 0);
+        //迭代
+        Stack<TreeNode> stack = new Stack<>();
+        if (root == null) {
+            return list;
+        }
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 145. 二叉树的后序遍历
+     * @param root 二叉树根节点
+     * @return 后序序遍历集合
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        treeNodeTraversal(list, root, 2);
+        return list;
+    }
+
+    /**
+     * 递归方式遍历二叉树
+     * @param list 遍历集合
+     * @param root 根节点
+     * @param flag 遍历方式
+     *             0   - >  前序遍历
+     *             1   - >  中序遍历
+     *             2  - >  后序遍历
+     */
+    private void treeNodeTraversal(List<Integer> list, TreeNode root, int flag){
+        if (root == null) {
+            return;
+        };
+        switch (flag) {
+            case 0:
+                list.add(root.val);
+                treeNodeTraversal(list, root.left, 0);
+                treeNodeTraversal(list, root.right, 0);
+                break;
+            case 1:
+                treeNodeTraversal(list, root.left, 1);
+                list.add(root.val);
+                treeNodeTraversal(list, root.right, 1);
+                break;
+            case 2:
+                treeNodeTraversal(list, root.left, 2);
+                treeNodeTraversal(list, root.right, 2);
+                list.add(root.val);
+                break;
         }
     }
 
